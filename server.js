ee06056864e995b875d0d7e59c5e09e10acdd435
chetandabli <hrku002@gmail.com>
2023-06-28 13:19:14 +0530

    initial deployment ready

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.use(express.json());
 
 const { Configuration, OpenAIApi } = require("openai");
 
-async function generateCompletion(language, code) {
+async function generateCompletion(input) {
   try {
-    const prompt = `Convert the following code:-  ${language} to:\n${code} code`;
+    const prompt = input;
     const maxTokens = 500;
     const n = 1;
 
@@ -43,13 +43,36 @@ async function generateCompletion(language, code) {
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Handle incoming requests to the /joke route
+// Handle incoming requests to the /convert route
 app.post('/convert', async (req, res) => {
   try {
-    const { code, language } = req.body;
+    const {code, language } = req.body;
     
-    let response = await generateCompletion(code, language);
-    // const joke = response.data.choices[0].text.trim();
+    let response = await generateCompletion(`Convert the following code:-  ${language} to:\n${code} code. \n if the code is incorrect or not complate please make gusses and complate it.`);
+    res.json({ response });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+// Handle incoming requests to the /debug route
+app.post('/debug', async (req, res) => {
+  try {
+    const {code} = req.body;
+    
+    let response = await generateCompletion(`Debug the following code:-  ${code} \n please check if there is any error and also correct it. also if it's correct provide steps what code is doing and how we can improve it`);
+    res.json({ response });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+// Handle incoming requests to the /quality route
+app.post('/quality', async (req, res) => {
+  try {
+    const {code} = req.body;
+    
+    let response = await generateCompletion(`Check the quality of the following code:-  ${code} \n please provide detailed info and also provide some tips to improve. provide in points`);
     res.json({ response });
   } catch (error) {
     console.error('Error:', error);
